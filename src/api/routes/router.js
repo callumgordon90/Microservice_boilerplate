34c385fed4167ed4fbe55628d37e7f666ebac08b
callum <cjoseph@aiudo.es>
2022-10-04 13:00:53 +0200

    created the template for the router file

---
+++ src/api/routes/router.js
@@ -0,0 +1,11 @@
+const carerController = require ('../controllers/carerController.js')
+const clientController = require ('../controllers/clientController.js')
+const newNumber = require ('../controllers/newNumber.js')
+
+
+const router = require ('express').Router()
+
+router.post(https://erp.aiudo.es/api/voip/call-in-progress)
+
+
+module.exports = router
